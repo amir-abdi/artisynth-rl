8c558849751640a2b828ed55c06dfda44a885455
amirabdi <amir.h.abdi@gmail.com>
2019-09-13 13:59:53 -0700

    Update model name in batch_artisynth and jaw2

--- scripts/jaw2.sh
+++ scripts/jaw2.sh
@@ -10,7 +10,7 @@ python3 src/python/main_baselines.py \
 --artisynth-model=jaw.RlJawDemo \
 --test=false \
 --port=8080 \
---artisynth-args="-disc true -condyleConstraints true" \
+--artisynth-args="-disc false -condyleConstraints true" \
 --include-current-pos=true \
 --wait-action=0.1 \
 --incremental_actions=True \
@@ -18,8 +18,6 @@ python3 src/python/main_baselines.py \
 --save-interval=1 \
 --log-interval=1 \
 --nsteps=2048 \
---num_timestep=20480 \
---load-path=/home/amirabdi/artisynth-rl/results/JawEnv-v0/ppo/checkpoints/00008 \
-
-#--use_wandb=false \
+--num_timestep=204800 \
+--use-wandb=true \
 
