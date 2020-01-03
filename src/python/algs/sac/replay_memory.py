e51ab6621cc276badf75958e91750e6a4595d94d
amirabdi <amir.h.abdi@gmail.com>
2020-01-02 17:27:24 -0800

    Add comments

--- src/python/algs/sac/replay_memory.py
+++ src/python/algs/sac/replay_memory.py
@@ -13,6 +13,9 @@ class ReplayMemory:
         self.position = 0
 
     def push(self, state, action, reward, next_state, done):
+        """Pushes the tuple into the memory and deletes oldest samples if the size of the memory
+        has reached its maximum capacity.
+        """
         if len(self.buffer) < self.capacity:
             self.buffer.append(None)
         self.buffer[self.position] = (state, action, reward, next_state, done)
